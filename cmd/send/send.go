cbe18cc128d6aba8e8e637d2ef755cb6aa054a78
Zachary Sohovich <zachary.sohovich@nike.com>
2021-01-11 10:38:52 -0800

    started conversion from rust to golang

---
+++ cmd/send/send.go
@@ -0,0 +1,21 @@
+package send
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/upload", func(c *gin.Context) {
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+
+	router.Run(":8080")
+}
